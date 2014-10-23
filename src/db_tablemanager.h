24e7e20323cf7d53e467159640d45114554a46f8
胡津铭 <hoojamis@gmail.com>
2014-10-23 21:20:00 +0800

    create table function finished

--- src/db_tablemanager.h
+++ src/db_tablemanager.h
@@ -33,7 +33,7 @@ private:
     static constexpr uint64 TABLE_NAME_LENGTH = 512;
     static constexpr uint64 FIELD_INFO_LENGTH = 256;
     static constexpr uint64 PAGE_HEADER_LENGTH = 24;
-    static constexpr uint64 FIRST_PAGE_INFO_PAGE = 2;
+    static constexpr uint64 FIRST_FIELDS_INFO_PAGE = 2;
     static constexpr uint64 FIRST_RECORD_PAGE = 5;
     static constexpr uint64 FIRST_EMPTY_SLOT_PAGE = 3;
     static constexpr uint64 FIRST_EMPTY_PAGE_PAGE = 4;
@@ -85,15 +85,26 @@ public:
         
         // create 1st page
         createDataDescriptionPage(table_name, fields, page_size, buffer);
+
         // write 1st page
         _file->writePage(1, buffer);
-
         
+        // create Fields description page(s), the first is 2nd page
+        createFieldsDescriptionPages(fields, page_size, buffer);
 
+        // write 2nd page
+        _file->writePage(2, buffer);
 
-        // TODO
-        // modify page number
-        // add table header
+        // create empty slots bitmap page, the first is 3rd page
+        // 0 means full, 1 means there's at least one empty slot
+        memset(buffer, 0x00, page_size);
+        _file->writePage(3, buffer);
+
+        // create empty pagess bitmap page, the first is 4nd page
+        // 0 means full, 1 means there's at least one empty slot
+        // memset(buffer, 0x00, page_size);
+        _file->writePage(4, buffer);
+        
 
 
         // delete DBFile
@@ -171,7 +182,10 @@ private:
         memcpy(buffer + pos, &page_size, sizeof(page_size));
         pos += sizeof(page_size);
         // number of pages existing in this file
-        uint64 num_pages = 1;
+        // 0th - file description, 1st - table description
+        // 2nd - fields description, 3rd - empty slots bitmap
+        // 4th - empty pages bitmap
+        uint64 num_pages = 4;
         memcpy(buffer + pos, &num_pages, sizeof(num_pages));
         pos += sizeof(num_pages);
         // align
@@ -188,19 +202,37 @@ private:
         // table name
         memcpy(buffer + pos, table_name.c_str(), TABLE_NAME_LENGTH); 
         pos += TABLE_NAME_LENGTH;
+
         // fields num
         uint64 fields_num = fields.size();
         memcpy(buffer + pos, &fields_num, sizeof(fields_num));
         pos += sizeof(fields_num);
+        /*
+        // too many fields that need more than one description page are not supported for now
         // number of filed description in each page
         uint64 field_info_each_page = (page_size - PAGE_HEADER_LENGTH) /
                                       FIELD_INFO_LENGTH;
         memcpy(buffer + pos, &field_info_each_page, sizeof(field_info_each_page));
         pos += sizeof(field_info_each_page);
+        */
         // first field description page
-        uint64 first_page_info_page = FIRST_PAGE_INFO_PAGE;
+        uint64 first_page_info_page = FIRST_FIELDS_INFO_PAGE;
         memcpy(buffer + pos, &first_page_info_page, sizeof(first_page_info_page));
         pos += sizeof(first_page_info_page);
+        
+        // first empty slot map page
+        uint64 first_empty_slot_page = FIRST_EMPTY_SLOT_PAGE;
+        memcpy(buffer + pos, &first_empty_slot_page, sizeof(first_empty_slot_page));
+        pos += sizeof(first_empty_slot_page);
+        // first empty page map page
+        uint64 first_empty_page_page = FIRST_EMPTY_PAGE_PAGE;
+        memcpy(buffer + pos, &first_empty_page_page, sizeof(first_empty_page_page));
+        pos += sizeof(first_empty_page_page);
+        // mapped pages in each page
+        uint64 pages_mapped_each_page = (page_size - PAGE_HEADER_LENGTH) * 8;
+        memcpy(buffer + pos, &pages_mapped_each_page, sizeof(pages_mapped_each_page));
+        pos += sizeof(pages_mapped_each_page);
+
         // record length
         uint64 record_length = fields.totalLength();
         memcpy(buffer + pos, &record_length, sizeof(record_length));
@@ -214,21 +246,34 @@ private:
         uint64 first_record_page = FIRST_RECORD_PAGE;
         memcpy(buffer + pos, &first_record_page, sizeof(first_record_page));
         pos += sizeof(first_record_page);
-        // first empty slot map page
-        uint64 first_empty_slot_page = FIRST_EMPTY_SLOT_PAGE;
-        memcpy(buffer + pos, &first_empty_slot_page, sizeof(first_empty_slot_page));
-        pos += sizeof(first_empty_slot_page);
-        // first empty page map page
-        uint64 first_empty_page_page = FIRST_EMPTY_PAGE_PAGE;
-        memcpy(buffer + pos, &first_empty_page_page, sizeof(first_empty_page_page));
-        pos += sizeof(first_empty_page_page);
-        // mapped pages in each page
-        uint64 pages_mapped_each_page = (page_size - PAGE_HEADER_LENGTH) * 8;
-        memcpy(buffer + pos, &pages_mapped_each_page, sizeof(pages_mapped_each_page));
-        pos += sizeof(pages_mapped_each_page);
         // align
         memset(buffer + pos, ALIGN, page_size - pos);
         // 1st page end
+    }
+
+    void createFieldsDescriptionPages(const DBFields& fields, 
+                                      uint64 page_size, char* buffer) {
+        // generate page header
+        auto page_header = makePageHeader(FIRST_FIELDS_INFO_PAGE, 0, 0);
+        assert(page_header.size() == PAGE_HEADER_LENGTH);
+
+        uint64 pos = 0;
+        // copy page header to buffer
+        memcpy(buffer + pos, page_header.data(), PAGE_HEADER_LENGTH);
+        pos += PAGE_HEADER_LENGTH;
+
+        // generate fields description
+        char field_info_buffer[FIELD_INFO_LENGTH];
+        for (uint64 i = 0; i < fields.size(); ++i) {
+            memset(field_info_buffer, ALIGN, FIELD_INFO_LENGTH);
+            fields.generateFieldDescription(i, field_info_buffer);
+            memcpy(buffer + pos, field_info_buffer, FIELD_INFO_LENGTH);
+            pos += FIELD_INFO_LENGTH;
+        }
+
+        // fields description larger than one page not supported for now
+        assert(pos <= page_size);
+        memset(buffer + pos, ALIGN, page_size - pos);
     }
 
     std::array<char, 3 * sizeof(uint64)> makePageHeader(const uint64 id, 
