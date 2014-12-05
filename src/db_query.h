9e02b5e35d4b4a681e36af8c727f186333883555
胡津铭 <hoojamis@gmail.com>
2014-12-05 21:13:56 +0800

    add query processor classes

---
+++ src/db_query.h
@@ -0,0 +1,53 @@
+/******************************************************************************
+ *  Copyright (c) 2014 Jinming Hu 
+ *  Distributed under the MIT license 
+ *  (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ *  
+ *  Project: Database
+ *  Filename: db_query.h 
+ *  Version: 1.0
+ *  Author: Jinming Hu
+ *  E-mail: hjm211324@gmail.com
+ *  Date: Dec. 05, 2014
+ *  Time: 21:01:37
+ *  Description: deal with a query. 
+                 Call anaylyser to analyse, then compile and execute.
+ *****************************************************************************/
+#ifndef DB_QUERY_H_
+#define DB_QUERY_H_
+
+#include "db_query_analyser.h"
+
+class Database::DBQuery {
+
+public:
+    bool execute(const std::string& str) {
+        using namespace QueryProcess;
+
+        auto ite_begin(std::begin(str));
+        auto ite_end(std::end(str));
+
+        CreateDBStatementParser<decltype(ite_begin), qi::space_type> p;
+        CreateDBStatement query;
+        
+        bool ok = qi::phrase_parse(ite_begin, ite_end, p, qi::space, query); 
+        if (ok) {
+            std::cout << "-------------------------\n";
+            std::cout << "Stmt: " << str << std::endl;
+            std::cout << "Parsing succeeded\n";
+            std::cout << "Get: [" << query.db_name << "]\n";
+            std::cout << "-------------------------\n";
+            return 0;
+        } else {
+            std::cout << "-------------------------\n";
+            std::cout << "Stmt: " << str << std::endl;
+            std::cout << "Parsing failed\n";
+            std::cout << "-------------------------\n";
+            return 1;
+        }
+    }
+
+};
+
+
+#endif /* DB_QUERY_H_ */
---
+++ src/db_query_analyser.h
@@ -0,0 +1,53 @@
+/******************************************************************************
+ *  Copyright (c) 2014 Jinming Hu 
+ *  Distributed under the MIT license 
+ *  (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
+ *  
+ *  Project: Database
+ *  Filename: db_query_analyser.h 
+ *  Version: 1.0
+ *  Author: Jinming Hu
+ *  E-mail: hjm211324@gmail.com
+ *  Date: Dec. 05, 2014
+ *  Time: 20:33:43
+ *  Description: Query analyser, parse a string statement.
+ *****************************************************************************/
+#ifndef DB_QUERY_ANALYSER_H_
+#define DB_QUERY_ANALYSER_H_
+
+#include <boost/fusion/adapted.hpp>
+#include <boost/spirit/include/qi.hpp>
+#include "db_common.h"
+
+namespace Database{
+namespace QueryProcess {
+using namespace boost::spirit;
+
+struct CreateDBStatement {
+    std::string db_name;
+};
+
+template <class Iterator, class Skipper = qi::space_type>
+struct CreateDBStatementParser: qi::grammar<Iterator, CreateDBStatement(), Skipper> {
+    CreateDBStatementParser(): CreateDBStatementParser::base_type(start) {
+        using namespace qi;
+
+        sql_identifier = lexeme[(alpha | char_('_')) >> *(alnum | char_('_'))];
+
+        start = no_case["create database"] >> omit[no_skip[+space]] >> sql_identifier >> ';';
+    }
+private:
+    qi::rule<Iterator, std::string(), Skipper> sql_identifier;
+    qi::rule<Iterator, CreateDBStatement(), Skipper> start;
+};
+
+
+}
+}
+
+BOOST_FUSION_ADAPT_STRUCT(::Database::QueryProcess::CreateDBStatement,
+                          (std::string, db_name))
+
+
+
+#endif /* DB_QUERY_ANALYSER_H_ */
