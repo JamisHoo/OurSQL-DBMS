0d144f3ee72e3b59b675515ed7a01c9610d5019e
胡津铭 <hoojamis@gmail.com>
2014-12-21 15:02:45 +0800

    rename main function file

--- test/oursql.cc
+++ test/oursql.cc
@@ -3,18 +3,18 @@
  *  Distributed under the MIT license 
  *  (See accompanying file LICENSE or copy at http://opensource.org/licenses/MIT)
  *  
- *  Project: 
- *  Filename: sqlfile_test.cc 
+ *  Project: Database
+ *  Filename: oursql.cc 
  *  Version: 1.0
  *  Author: Jamis Hoo
  *  E-mail: hjm211324@gmail.com
  *  Date: Dec. 09, 2014
  *  Time: 21:43:46
- *  Description: 
+ *  Description: main function of total project.
+ *               read in sql statements and pass them to query parser.
  *****************************************************************************/
 #include <iostream>
 #include <fstream>
-#include <regex>
 #include "../src/db_query.h"
 #include "../src/db_interface.h"
 
@@ -33,24 +33,21 @@ int main(int argc, char** argv) {
     if (argc == 2)
         fin.open(argv[1]);
     
-    // if stdin mode, output prompt
-    if (argc == 1) std::clog << "yoursql> " << std::flush;
-
     std::string str;
-    while (std::getline(argc == 1? std::cin: fin, str)) {
+    while (true) {
+        // if stdin mode, output prompt
+        if (argc == 1) 
+            std::clog << (ui.emptyBuff()? "oursql> ": "      > ") << std::flush;
+
         // read a line
+        if (!std::getline(argc == 1? std::cin: fin, str)) break;
+
         ui.feed(str + '\n');
 
         // fetch commands and execute
         while (ui.ready()) query.execute(ui.get());
-
-        // if stdin mode, output prompt
-        if (argc == 1) {
-            if (ui.emptyBuff()) std::clog << "yoursql> " << std::flush;
-            else std::clog << "       > " << std::flush;
-        }
     }
     if (argc == 1) std::clog << std::endl;
     
-    
+    return 0; 
 } 
